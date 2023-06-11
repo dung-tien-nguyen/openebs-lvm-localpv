de513813fb01a66b44eef0c909a42acafd5b51bc
Nguyen Tien Dung <dung.tiennguyen1906@gmail.com>
2023-06-11 07:54:32 +0000

    edit ci.yaml - 03

--- ci/ci-test.sh
+++ ci/ci-test.sh
@@ -83,6 +83,8 @@ waitForLVMDriver() {
     # echo get pod
     echo "################### Dungnt list all pod ###################"
     kubectl get pod -A --show-labels
+    kubectl -n kube-system describe pod openebs-lvm-controller-0
+    kubectl -n kube-system logs pod openebs-lvm-controller-0 -c openebs-lvm-plugin
     echo "################### Dungnt list all pod ###################"
     i=$(( i + interval ))
     echo "Waiting for lvm-driver to be ready..."
